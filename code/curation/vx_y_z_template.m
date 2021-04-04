50793726de7108866c5de7f1165714fda8c41142
Eduard Kerkhoven <eduardk@chalmers.se>
2021-04-04 22:04:50 +0200

    doc: small changes in curation template

--- code/curation/vx_y_z_template.m
+++ code/curation/vx_y_z_template.m
@@ -1,7 +1,7 @@
 %% Curations for release version x.y.z
 % This script combines various curations on iYali vx.y.y to produce vx.y.z.
-% Indicate is which issues are solved, more detailed explanation is given in the
-% relevant pull requests
+% Indicated is which issues are solved, more detailed explanation is given
+% in the relevant pull requests.
 
 %% Define root of repository, requires git to be installed
 [status,root]=system('git rev-parse --show-toplevel');
@@ -13,12 +13,13 @@ cd([root, '/code'])
 model = getEarlierModel('4.1.1'); % Modify to fit relevant model version
 
 %% Fixes Issue #xx
-% Include code here that fixes the issue. If there is a lot of code, or whenever
-% relevant for other reasons, you can also call other scripts from here.
+% Include code here that fixes the issue. If there is a lot of code, or
+% whenever relevant for other reasons, you can also call other scripts from
+% here.
 
 %% Fixes Issue #yy
-% Multiple issues can be fixed in one release, but keep the code separate so
-% it can easily be traced back to the relevant issue.
+% Multiple issues can be fixed in one release, but keep the code separate
+% so it can easily be traced back to the relevant issue.
 
 %% Prepare necessary files before making pull-request to the `devel` branch
 cd([root, '/code'])

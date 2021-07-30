6f15e04261f7825642f78f02945ca540eaa41c35
Mihail Anton <mihail.anton@chalmers.se>
2021-07-30 07:38:18 +0200

    chore: rename master to main across the repo

--- code/newRelease.m
+++ code/newRelease.m
@@ -6,10 +6,10 @@ function newRelease(bumpType)
 %   bumpType    string specifying the type of release, either 'major',
 %               'minor' or 'patch'
 
-%Check if in master:
+%Check if in main:
 currentBranch = git('rev-parse --abbrev-ref HEAD');
-if ~strcmp(currentBranch,'master')
-    error('ERROR: not in master')
+if ~strcmp(currentBranch,'main')
+    error('ERROR: not in main')
 end
 
 %Bump version number:
